06c99693420aee2e1d7c0badcc1cce698f15e5e7
c0derbr1t <c0der.br1t@gmail.com>
2020-03-02 15:53:37 -0600

    Changed production knexfile to sqlite3

--- knexfile.js
+++ knexfile.js
@@ -45,11 +45,14 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: 'sqlite3',
+    connection: {
+      filename: process.env.DATABASE_URL
+    },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     },
     migrations: {
       tableName: 'knex_migrations',
